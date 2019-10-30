8590e3461a5e2d2914434a9fa70d75e95468248d
Luca Steeb <contact@luca-steeb.com>
2019-10-30 23:45:40 +0100

    fix(build): also push latest tag

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ export PRISMA=1.34.10
 
 export tag="go_v$GO-prisma_v$PRISMA"
 
-docker build -t "steebchen/go-prisma:$tag" --build-arg "GO_VERSION=$GO" --build-arg "PRISMA_VERSION=$PRISMA" .
+docker build -t "go-prisma:$tag" --build-arg "GO_VERSION=$GO" --build-arg "PRISMA_VERSION=$PRISMA" .
+docker tag "go-prisma:$tag" "steebchen/go-prisma:$tag"
+docker tag "go-prisma:$tag" "steebchen/go-prisma:latest"
 docker push "steebchen/go-prisma:$tag"
+docker push "steebchen/go-prisma:latest"
